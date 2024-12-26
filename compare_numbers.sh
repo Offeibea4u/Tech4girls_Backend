61e3cda672a011c5fd5d573bb18808cc3336b5fe
Offeibea <offeibea4u@gmail.com>
2024-12-26 16:31:49 +0000

    task 6

---
+++ compare_numbers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+num1=$1
+num2=$2
+
+
+if [ $num1 -gt $num2 ]; then
+    echo "$num1 is greater than $num2."
+elif [ $num1 -lt $num2 ]; then
+    echo "$num1 is less than $num2."
+else
+    echo "$num1 and $num2 are equal."
+fi
